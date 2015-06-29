2b7aa41f5c9a2f298da47803d43e3282cca73dc2
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-06-29 16:07:33 +0200

    Initial commit

---
+++ ngx/_build/exception-handler.decorator.js
@@ -0,0 +1,43 @@
+export default function(ngModule, options){
+
+  ngModule.config(registerExceptionHandler);
+
+  /**
+   * This decorator allows custom logic to be executed
+   * when an exception is thrown.
+   *
+   * By default, it first delegates to the built-in
+   * AngularJS exception handler that logs a stacktrace
+   * in the console.
+   *
+   * @param $delegate
+   * @returns {Function}
+   */
+  function exceptionHandler($delegate){
+    return function(exception, cause) {
+
+      // Delegate to AngularJS handler first
+      $delegate(exception, cause);
+
+      // PUT YOUR CUSTOM CODE RIGHT HERE
+
+    };
+  }
+
+  // Inject dependencies
+  exceptionHandler.$inject = ['$delegate'];
+
+  /**
+   * Register the exception handler with the provider
+   *
+   * @param $provide
+   */
+  function registerExceptionHandler($provide){
+    $provide.decorator('$exceptionHandler', exceptionHandler);
+  }
+
+  // Inject dependencies
+  registerExceptionHandler.$inject = ['$provide'];
+
+};
+
